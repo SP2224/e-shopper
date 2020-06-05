0666d9b0f5590a730aabedceeb1df76e81f3dde9
Santanu Roy <santanu.roy32@gmail.com>
2020-06-05 23:59:02 +0530

    Added more items to the product in the database

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView
 
+from .models import *
+
 # Create your views here.
-class HomeView(TemplateView):
+class HomeView(ListView):
+    model = Product
     template_name = 'index.html'
